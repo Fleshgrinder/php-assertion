d82d82eafc3c66291403d69f3c174982679e9a8c
Richard Fussenegger <fleshgrinder@users.noreply.github.com>
2017-01-26 16:57:53 +0100

    Another attempt to keep code DRY without errors

--- bin/travis/after_success.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset -o errexit -o noclobber -o noglob -o pipefail -o privileged
-
-travis_retry coveralls --no-interaction &
-travis_retry test-reporter --no-interaction &
-travis_retry ocular code-coverage:upload --format=php-clover build/logs/clover.xml &
-wait
